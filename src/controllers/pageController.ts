63ff23efe25a061ee09d0b6fc1e4501aeedb63ee
winnysan <maarvinko@gmail.com>
2024-03-11 15:00:46 +0100

    auth nav

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,23 +1,27 @@
 import express from 'express'
 
 const homePage = async (req: express.Request, res: express.Response) => {
-  res.render('index', { user: req.session.user, messages: req.flash('info') })
+  res.render('index', {
+    user: req.session.user,
+    messages: req.flash('info'),
+    title: 'Home page',
+  })
 }
 
 const registerPage = async (req: express.Request, res: express.Response) => {
-  res.render('register')
+  res.render('register', { user: req.session.user, title: 'Register page' })
 }
 
 const loginPage = async (req: express.Request, res: express.Response) => {
-  res.render('login')
+  res.render('login', { user: req.session.user, title: 'Login page' })
 }
 
 const adminPage = async (req: express.Request, res: express.Response) => {
-  res.render('admin', { user: req.session.user })
+  res.render('admin', { user: req.session.user, title: 'Admin page' })
 }
 
 const dashboardPage = async (req: express.Request, res: express.Response) => {
-  res.render('dashboard', { user: req.session.user })
+  res.render('dashboard', { user: req.session.user, title: 'Dashboard page' })
 }
 
 export { adminPage, dashboardPage, homePage, loginPage, registerPage }
