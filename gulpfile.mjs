462979a6d036c77a9082900a35de46f56325bb34
winnysan <maarvinko@gmail.com>
2024-08-31 12:15:54 +0200

    scss gulp

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -4,6 +4,7 @@ import fs from 'fs'
 import { dest, series, src, task } from 'gulp'
 import pkg from 'gulp-typescript'
 import minimist from 'minimist'
+import * as sass from 'sass'
 
 const { createProject } = pkg
 const tsProject = createProject('tsconfig.json')
@@ -61,7 +62,7 @@ task('views-update', done => {
 
     const manifest = JSON.parse(data)
     const scriptTag = `<script type="text/javascript" src="${manifest['/js/script.js']}" defer></script>`
-    const styleTag = `<link rel="stylesheet" href="${manifest['/css/style.css']}">`
+    const styleTag = `<link rel="stylesheet" href="${manifest['/css/style.css']}" />`
 
     fs.readFile(ejsFilePath, 'utf8', (err, fileData) => {
       if (err) {
@@ -71,6 +72,47 @@ task('views-update', done => {
       const updatedFileData = fileData
         .replace(/<script type="text\/javascript" src="\/js\/.*\.js" defer><\/script>/, scriptTag)
         .replace(/<link rel="stylesheet" href="\/css\/.*\.css" \/>/, styleTag)
+
+      fs.writeFile(ejsFilePath, updatedFileData, 'utf8', err => {
+        if (err) {
+          return done(err)
+        }
+
+        done()
+      })
+    })
+  })
+})
+
+/**
+ * compile style
+ */
+task('style-compile', done => {
+  const timestamp = Math.floor(Date.now() / 1000)
+  const cssFilename = `style.${timestamp}.css`
+  const scssFilePath = './src/public/css/style.scss'
+  const cssOutputPath = `./dist/public/css/${cssFilename}`
+  const ejsFilePath = './dist/views/layouts/main.ejs'
+
+  const result = sass.renderSync({
+    file: scssFilePath,
+    outputStyle: 'expanded',
+  })
+
+  fs.writeFile(cssOutputPath, result.css, err => {
+    if (err) {
+      return done(err)
+    }
+
+    fs.readFile(ejsFilePath, 'utf8', (err, fileData) => {
+      if (err) {
+        return done(err)
+      }
+
+      const updatedFileData = fileData.replace(
+        /<link rel="stylesheet" href="\/css\/.*\.css" \/>/,
+        `<link rel="stylesheet" href="/css/${cssFilename}" />`
+      )
 
       fs.writeFile(ejsFilePath, updatedFileData, 'utf8', err => {
         if (err) {
